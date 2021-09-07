652bf49a6a75aa4c0bd46a803e9a0e629eb4dc5a
Rayen-cherni <rayen.cherni1160@gmail.com>
2021-09-07 03:02:39 +0200

    Report request progress

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,8 @@
+import { HttpErrorResponse, HttpEvent, HttpEventType } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { FileService } from '../service/file.service';
+import { fileSaver } from 'file-saver';
 
 @Component({
   selector: 'app-home',
@@ -8,14 +11,91 @@ import { Router } from '@angular/router';
 })
 export class HomeComponent implements OnInit {
 
-  constructor(private router: Router) { }
+
+  filenames: String[] = [];
+  fileStatus= {status : "",requestType:"",percent :0};
+
+  constructor(private router: Router,
+    private fileService: FileService) { }
 
   ngOnInit(): void {
   }
+
+  //Define a function to upload files 
+  onUploadFiles(files: File[]): void {
+    const formData = new FormData();
+    for (const file of files) {
+      formData.append('files', file, file.name);
+    }
+    this.fileService.upload(formData).subscribe(
+      (result) => {
+        console.log(result);
+        this.reportProgress(result);
+      },
+      (error: HttpErrorResponse) => {
+        console.log(error);
+      }
+    )
+  }
+
+  //Define a function to download files 
+  onDownloadFiles(filename: String): void {
+
+    this.fileService.download(filename).subscribe(
+      (result) => {
+        console.log(result);
+        this.reportProgress(result);
+      },
+      (error: HttpErrorResponse) => {
+        console.log(error);
+      }
+    )
+  }
+
 
   logout() {
     this.router.navigate(['/login']);
 
   }
 
+  private reportProgress(httpEvent: HttpEvent<String[] | Blob>): void {
+    switch (httpEvent.type) {
+      case HttpEventType.UploadProgress:
+        this.updateStatus(httpEvent.loaded, httpEvent.total, 'Uploading');
+        break;
+      case HttpEventType.DownloadProgress:
+        this.updateStatus(httpEvent.loaded, httpEvent.total, 'Downloading');
+        break;
+      case HttpEventType.ResponseHeader:
+        console.log('Header returned', httpEvent);
+        break;
+      case HttpEventType.Response:
+        if (httpEvent.body instanceof Array) {
+          for (const filename of httpEvent.body) {
+            //unshoft is the similar of push but here we add the item on the beginning 
+            this.filenames.unshift(filename);
+          }
+        } else {
+          //Download Logic  
+          //we will use file saver to save files on the client browser 
+          fileSaver(new File([httpEvent.body], httpEvent.headers.get('File-Name'),
+            { type: `${httpEvent.headers.get('Content-Type')};charset=utf-8` }));
+
+        }
+        break;
+      default:
+        console.log(httpEvent);
+          break;
+    }
+  }
+
+
+
+  updateStatus(loaded: number, total: number, requestType: string) {
+    this.fileStatus.status = 'progress';
+    this.fileStatus.requestType = requestType;
+    this.fileStatus.percent = Math.round(100 * loaded / total);
+  }
 }
+
+
--- src/app/service/file.service.ts
+++ src/app/service/file.service.ts
@@ -1,9 +1,32 @@
+import { HttpClient, HttpEvent } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FileService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  apiUrl = 'http://localhost:8080';
+
+  //Define function to upload files 
+  upload(formData: FormData): Observable<HttpEvent<String[]>> {
+    return this.http.post<String[]>(`${this.apiUrl}/uploader/api/file/upload`, formData, {
+      reportProgress: true, //report all the progress of the request 
+      observe: 'events'
+    })
+  }
+ 
+  //Define function to download files 
+ download(filename: String): Observable<HttpEvent<Blob>> {
+  return this.http.get(`${this.apiUrl}/uploader/api/file/download/${filename}`, {
+    reportProgress: true, //report all the progress of the request 
+    observe: 'events',
+    responseType:'blob'
+  })
+}
+
 }
